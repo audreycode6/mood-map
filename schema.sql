eefa7eddee2a6a5b2c21b5b53de458d1f4ab9b73
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-02-25 14:14:53 -0800

    fix schema from typos

--- schema.sql
+++ schema.sql
@@ -9,12 +9,12 @@ user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 entry_date DATE NOT NULL DEFAULT CURRENT_DATE, 
 energy_level int NOT NULL CHECK (energy_level BETWEEN 1 AND 5),
 mood_range int NOT NULL CHECK (mood_range BETWEEN 1 AND 5),
-reflection text
+reflection text,
 UNIQUE (user_id, entry_date)
 );
 
 CREATE TABLE emotions (
 id serial PRIMARY KEY,
-entry_id int NOT NULL REFERENCES entries(id) ON DELETE CASCADE;
+entry_id int NOT NULL REFERENCES entries(id) ON DELETE CASCADE,
 emotion text NOT NULL
 );
