742a8bfd297a82d30f5b51f0aabdd11c75ea5f47
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-03-11 14:10:11 -0700

    add helper funcs for input validation in routes

--- app.py
+++ app.py
@@ -28,11 +28,16 @@ ____________
 
 
 def is_logged_in():
+    """returns username if user is currently logged in"""
     return session.get("username")
 
 
 def get_valid_page_nums():
-    # get total of entries
+    """
+    returns list of pages for view_entries pagination
+     - determined by users total entry count +
+       the # of pages shown in a view_entry page (ENTRY_VIEW_LIMIT)
+    """
     total_entries = g.storage.get_user_entry_count(session["user_id"])
     entry_range = list(range(1, total_entries[0] + 1, ENTRY_VIEW_LIMIT))
     valid_page_nums = []
@@ -43,12 +48,18 @@ def get_valid_page_nums():
     return valid_page_nums
 
 
-def validate_entry(entry_date, energy_level, mood_range):
+def validate_entry(entry_date, energy_level, mood_range, edit_entry_id=False):
+    """
+    return error_list: empty if valid input or string for each error
+    errors:
+        - missing input for entry_date, energy_level, mood_range
+        - entry_date is not unique to user (already have an entry for that date)
+    """
     error_list = []
     if not entry_date:
         error_list.append("Missing a date")
     if entry_date:  # check that entry date for that user doesnt already exists
-        if g.storage.check_unique_date(session["user_id"], entry_date):
+        if validate_entry_date(entry_date, edit_entry_id):
             error_list.append(f"You already have an entry for this date: {entry_date}.")
     if not energy_level:
         error_list.append("Missing a value for energy level")
@@ -58,24 +69,21 @@ def validate_entry(entry_date, energy_level, mood_range):
     return error_list
 
 
-def validate_edited_entry(entry_id, entry_date, energy_level, mood_range):
-    error_list = []
-    if not entry_date:
-        error_list.append("Missing a date")
-    if entry_date:
+def validate_entry_date(entry_date, entry_id):
+    """
+    check entry_date is unique:
+    i.e belongs to current entry being editted / or is new unique date
+    if entry_id -> entry is being edited and
+        need to check if current entry_date matches input entry_date
+    returns None if valid entry_date
+    else a date (i.e date already exists for the users entries)
+    """
+    if entry_id:
         entry_date_is_same = g.storage.get_entry_date(entry_id)[0]
-        if str(entry_date_is_same) != entry_date:  # if not current entry_date value
-            # check that entry date for that user doesnt already exists
-            if g.storage.check_unique_date(session["user_id"], entry_date):
-                error_list.append(
-                    f"You already have an entry for this date: {entry_date}."
-                )
-    if not energy_level:
-        error_list.append("Missing a value for energy level")
-    if not mood_range:
-        error_list.append("Missing a value for mood range")
+        if str(entry_date_is_same) == entry_date:  # valid date
+            return None
 
-    return error_list
+    return g.storage.check_unique_date(session["user_id"], entry_date)
 
 
 def validate_unique_emotions(emotions_string, error_list):
@@ -87,6 +95,36 @@ def validate_unique_emotions(emotions_string, error_list):
         else:
             count_emotions[emotion] = 1
     return error_list
+
+
+def validate_registration(username, password):
+    error_list = []
+    user_exists = g.storage.user_exists(username)
+    valid_user_length = len(username) <= 30
+
+    if not valid_user_length:
+        error_list.append(
+            f"User '{username}' is too long. Please enter a username shorter than 30 characters."
+        )
+
+    if user_exists:
+        error_list.append(f"User '{username}' already exists")
+
+    if not username or not password:
+        error_list.append("Missing input")
+
+    return error_list
+
+
+def validate_login(username, password):
+    if username and password:  # not missing input
+        result = g.storage.user_exists(username)
+        if result:  # valid user
+            hashed_pw = result[2].encode("utf-8")
+            b_pw = password.encode("utf-8")
+            if checkpw(b_pw, hashed_pw):  # user input pw matches hashed pw
+                return result[0]
+    return False
 
 
 def require_login(func):
@@ -124,35 +162,22 @@ def register():
     username = request.form.get("username").strip().lower()
     password = request.form.get("password").strip()
 
-    error_list = []
-    user_exists = g.storage.user_exists(username)
+    # validate username and pw
+    error_list = validate_registration(username, password)
+    if error_list:
+        for error in error_list:
+            flash(error, "error")
+        return render_template("register.html", username=username), 422
 
-    valid_user_length = len(username) <= 30
+    # valid registration
+    hashed_pw = hashpw(password.encode("utf-8"), gensalt())
+    # convert to str before storing in db
+    hashed_pw_str = hashed_pw.decode("utf-8")
 
-    if not valid_user_length:
-        error_list.append(
-            f"User '{username}' is too long. Please enter a username shorter than 30 characters."
-        )
-
-    if user_exists:
-        error_list.append(f"User '{username}' already exists")
-
-    if not username or not password:
-        error_list.append("Missing input")
-
-    if (username and password) and (not user_exists) and (valid_user_length):
-        hashed_pw = hashpw(password.encode("utf-8"), gensalt())
-        # convert to str before storing in db
-        hashed_pw_str = hashed_pw.decode("utf-8")
-
-        # create user
-        g.storage.register_new_user(username, hashed_pw_str)
-        flash(f"'{username}' has been registered!", "success")
-        return redirect(url_for("display_login"))
-
-    for error in error_list:
-        flash(error, "error")
-    return render_template("register.html", username=username), 422
+    # create user
+    g.storage.register_new_user(username, hashed_pw_str)
+    flash(f"'{username}' has been registered!", "success")
+    return redirect(url_for("display_login"))
 
 
 @app.route("/")
@@ -166,27 +191,24 @@ def login():
     password = request.form.get("password").strip()
 
     # validate input
-    if username and password:
-        result = g.storage.user_exists(username)
-        if result:
-            hashed_pw = result[2].encode("utf-8")
-            b_pw = password.encode("utf-8")
-            if checkpw(b_pw, hashed_pw):
-                # store username in session to track logged in user
-                session["username"] = username
-                session["user_id"] = result[0]
+    user_id = validate_login(username, password)
+    if not user_id:
+        # error redirect to same page with flash message
+        flash("Invalid credentials, please try again", "error")
+        return render_template("login.html"), 404
 
-                # if proceeding from login redirect go back to original path
-                protected_page = session.pop("protected_page_path", None)
-                if protected_page:
-                    return redirect(protected_page)
+    # valid login
+    # store username in session to track logged in user
+    session["username"] = username
+    session["user_id"] = user_id
 
-                # default
-                return redirect(url_for("view_entries"))
+    # if proceeding from login redirect go back to original path
+    protected_page = session.pop("protected_page_path", None)
+    if protected_page:
+        return redirect(protected_page)
 
-    # error redirect to same page with flash message
-    flash("Invalid credentials, please try again", "error")
-    return render_template("login.html"), 404
+    # default
+    return redirect(url_for("view_entries"))
 
 
 @app.route("/logout", methods=["POST"])
@@ -201,15 +223,15 @@ def logout():
 @require_login
 def view_entries(page_num):
     try:
-        user_id = session["user_id"]
-        entries_info = g.storage.get_users_entries_ids_and_date(
-            user_id, page_num, ENTRY_VIEW_LIMIT
-        )
-        valid_page_nums_list = get_valid_page_nums()
         # validate page view of entries
+        valid_page_nums_list = get_valid_page_nums()
         if page_num != 0 and page_num not in valid_page_nums_list:
-            raise ValueError(f"Page {page_num} out of entry views range")
+            raise ValueError(f"Page {page_num} out of Entry Views range")
 
+        # valid page view
+        entries_info = g.storage.get_users_entries_ids_and_date(
+            session["user_id"], page_num, ENTRY_VIEW_LIMIT
+        )
         session["page_num"] = page_num
         return render_template(
             "view_entries.html",
@@ -239,6 +261,7 @@ def create_entry():
 
     user_id = session["user_id"]
     try:
+        # validate input
         error_list = validate_entry(entry_date, energy_level, mood_range)
         if error_list:
             for error in error_list:
@@ -254,7 +277,7 @@ def create_entry():
                 ),
                 404,
             )
-
+        # valid input
         entry_id = g.storage.create_new_entry(
             user_id, entry_date, energy_level, mood_range, reflection
         )
@@ -286,7 +309,7 @@ def display_entry(entry_id):
             emotions=emotions_list,
         )
     except TypeError:
-        flash("Unauthorized entry id", "error")
+        flash(f"Unauthorized entry id: {entry_id}", "error")
         return redirect(url_for("view_entries")), 404
 
 
@@ -308,7 +331,7 @@ def display_edit_entry(entry_id):
             emotions=" ".join(emotions_list),
         )
     except TypeError:
-        flash("Unauthorized entry id", "error")
+        flash(f"Unauthorized entry id: {entry_id}", "error")
         return (
             redirect(url_for("view_entries")),
             404,
@@ -325,9 +348,7 @@ def edit_entry_and_emotions(entry_id):
     emotions_string = request.form.get("emotions")
 
     try:
-        error_list = validate_edited_entry(
-            entry_id, entry_date, energy_level, mood_range
-        )
+        error_list = validate_entry(entry_date, energy_level, mood_range, entry_id)
         if emotions_string:
             error_list = validate_unique_emotions(emotions_string, error_list)
         if error_list:
@@ -350,9 +371,9 @@ def edit_entry_and_emotions(entry_id):
             entry_id, entry_date, energy_level, mood_range, reflection
         )
         """
-            in order to edit emotions list need to
-            delete all current emotions to ensure deletions
-            in edit mode are applied accurately
+        in order to edit emotions_string need to
+        delete all current emotions to ensure deletions/additions
+        in edit mode are applied accurately
         """
         g.storage.delete_entries_emotions(entry_id)
         g.storage.add_emotions(entry_id, emotions_string)
@@ -366,8 +387,9 @@ def edit_entry_and_emotions(entry_id):
 @app.route("/delete_entry/<int:entry_id>", methods=["POST"])
 @require_login
 def delete_entry(entry_id):
+    entry_date = g.storage.get_entry_date(entry_id)
     g.storage.delete_entry(entry_id)
-    flash("Successfully deleted entry...", "success")
+    flash(f"Successfully deleted entry from: {entry_date[0]}", "success")
     return redirect(f"/view_entries/{session['page_num']}")
 
 
@@ -378,6 +400,7 @@ def delete_emotion(entry_id, emotion):
         emotion_id = g.storage.get_emotions_id(emotion, entry_id)
         if emotion_id is None:
             raise TypeError()
+
         g.storage.delete_emotion(emotion_id, entry_id)
         id, user_id, date, energy_level, mood_range, reflection = g.storage.get_entry(
             entry_id
